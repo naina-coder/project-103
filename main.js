2e04820b8a17859766c05e7a99040269e638fb12
naina-coder <82169500+naina-coder@users.noreply.github.com>
2021-11-09 16:44:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 //https://teachablemachine.withgoogle.com/models/Md4pokhYk/model.json
+
+Webcam.set({
+    width:350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera= document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+        console.log("!Clicked snap!");
+    });
+
+}
+
