389fe603cb79456d2636ca8fdcc64847f9e7dfd3
naina-coder <82169500+naina-coder@users.noreply.github.com>
2022-02-19 11:56:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,33 @@ function take_snapshot()
     Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
         console.log("!Clicked snap!");
+        classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Md4pokhYk/model.json',modelLoaded)
     });
+
+function modelLoaded()
+{
+    console.log("model loaded")
+}
+
+}
+
+function identify_snapshot(){
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.error(error);
+    }
+
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML  = results[0].confidence.toFixed(3);
+    }
+
 
 }
 
